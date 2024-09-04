95509fa68879f968b0a16774e796429548666357
Bulbo <gabrielshot1@gmail.com>
2024-09-04 13:35:40 -0300

    inicializado o spring

---
+++ src/main/java/com/wowcreations/roupaapi/RoupaapiApplication.java
@@ -0,0 +1,13 @@
+package com.wowcreations.roupaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoupaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoupaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wowcreations/roupaapi/RoupaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wowcreations.roupaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoupaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
